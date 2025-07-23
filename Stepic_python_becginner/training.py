53e8cadeb6eabd9d4fbec9009061dc99a47f8907
Volodymyr Kovalenko <severskwladimir@gmail.com>
2025-07-23 11:45:34 +0200

    create Strings folder and code in it

--- Stepic_python_becginner/training.py
+++ Stepic_python_becginner/training.py
@@ -1,7 +1,2 @@
-# 10, 5, 0.5, = 100
-#  10 20 200
-for i in range(1, 11):
-    for j in range(1, 21):
-        for k in range(1, 201):
-            if 10 * i + 5 * j + 0.5 * k == 100 and i + j + k == 100:
-                print("i =", i, "j =", j, "k =", k)
+print('2024-05-19'.islower())
+print('2024-05-19'.isupper())
